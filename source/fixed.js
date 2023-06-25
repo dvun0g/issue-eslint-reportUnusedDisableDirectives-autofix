d6552b5bb6a647b90c53027525b5c39f2bff3ca9
dvun0g <nikita.runikita.ru@gmail.com>
2023-06-26 00:49:42 +0300

    added eslint example autofix - reportUnusedDisableDirectives

---
+++ source/fixed.js
@@ -0,0 +1,11 @@
+// eslint no-alert: "off"
+/* eslint no-alert: "off" */
+
+/* eslint-enable no-alert, no-console */
+
+/* eslint-enable
+    no-alert,
+    no-console
+*/
+
+/* eslint-enable */
---
+++ source/index.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line
+/* eslint-disable-next-line */
+
+// eslint-disable-line
+/* eslint-disable-line */
+
+// eslint no-alert: "off"
+/* eslint no-alert: "off" */
+
+/* eslint-disable no-alert, no-console */
+/* eslint-enable no-alert, no-console */
+
+/* eslint-disable
+    no-alert,
+    no-console
+*/
+/* eslint-enable
+    no-alert,
+    no-console
+*/
+
+/* eslint-disable */
+/* eslint-enable */
